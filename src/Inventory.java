2f7c9885ad24837d022a9829bad948ec8914c9b2
DrScott <dougscott2@gmail.com>
2015-10-12 15:05:38 -0400

    submission for hw

--- src/Inventory.java
+++ src/Inventory.java
@@ -8,16 +8,13 @@ public class Inventory {
         int inventoryNum = 1;
 
         for (InventoryList inventory : list){
-
             System.out.println( inventoryNum + ". [" + inventory.quantity + "] " + inventory.item);
             inventoryNum++;
         }
     }
-
-    public static void main(String[] args){
+    public static void main(String[] args){  //that sweet sweet main class
         Scanner scanner = new Scanner(System.in);
         ArrayList<InventoryList> inventory = new ArrayList();
-
 
         while(true){   //infinite loop
             printInventory(inventory);
@@ -25,12 +22,10 @@ public class Inventory {
             System.out.println("[1] Create new inventory item");
             System.out.println("[2] Remove item");
             System.out.println("[3] Update item quantity");
-
-
+            //get user info
             String option = scanner.nextLine();
             int optionNum = Integer.valueOf(option);
-
-
+            //begin master if sequence
             if(optionNum == 1){
                 //create new inventory code
                 System.out.println("What would you like to add?");
@@ -40,34 +35,25 @@ public class Inventory {
                 String amount = scanner.nextLine();
                 int newQuantity = Integer.valueOf(amount);
                 list.quantity = newQuantity;
-
-
                 inventory.add(list);
-
             }
             else if (optionNum == 2){
-                //delete inventory code
+
                 System.out.println("Which item would you like to remove?");
                 String select = scanner.nextLine();
                 int selectNum = Integer.valueOf(select);
                 InventoryList list = inventory.remove(selectNum - 1);
-
             }
             else if (optionNum == 3){
                 System.out.println("Which item would you like to change?");
                 String select = scanner.nextLine();
                 int selectNum = Integer.valueOf(select);
                 InventoryList list = inventory.get(selectNum - 1);
-
-                // selectNum - 1 to keep it in line with the array
                 System.out.println("You currently have " + list.quantity + " " + list.item);
                 System.out.println("What is the new amount?");
                 select = scanner.nextLine();
                 int newQuantity = Integer.valueOf(select);
                 list.quantity = newQuantity;
-
-
-
             }
         }
     }
